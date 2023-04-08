b0ac66385358eb5a10d197ca2674baaa02a5d576
Arumugam J <sixface@msn.com>
2023-04-08 08:29:52 -0400

    Fixing repeated crashes in production

--- app/src/main/java/org/qosp/notes/data/Backup.kt
+++ app/src/main/java/org/qosp/notes/data/Backup.kt
@@ -1,17 +1,15 @@
 package org.qosp.notes.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.qosp.notes.data.model.IdMapping
-import org.qosp.notes.data.model.Note
-import org.qosp.notes.data.model.NoteTagJoin
-import org.qosp.notes.data.model.Notebook
-import org.qosp.notes.data.model.Reminder
-import org.qosp.notes.data.model.Tag
+import org.qosp.notes.data.model.*
 
 @Serializable
+@Parcelize
 data class Backup(
     val version: Int,
     val notes: Set<Note> = setOf(),
@@ -20,7 +18,7 @@ data class Backup(
     val tags: Set<Tag> = setOf(),
     val joins: Set<NoteTagJoin> = setOf(),
     val idMappings: Set<IdMapping> = setOf(),
-) {
+) : Parcelable {
     fun serialize() = Json.encodeToString(this)
 
     companion object {
